06722cd5a27134dbeeeb5cf0f9070c3efe85bcdb
A. Ganesh <a.ganesh.2@student.rug.nl>
2022-10-19 22:48:58 +0200

    Added live dashboard

--- web/web.py
+++ web/web.py
@@ -4,13 +4,15 @@ import time
 import json
 import requests
 import os
+import asyncio
 
 api_port = os.environ['APISERV_PORT']
 
 title_placeholder = st.empty()
 
 
-def upload_submission():
+async def upload_submission():
+
 
     assignments = requests.get(url=f'http://apiserv:{api_port}/getassignments/')
     assignments = assignments.json()
@@ -33,86 +35,109 @@ def upload_submission():
             'data'          : code_lines             
         }
 
-        try:
 
-            res = requests.post(url=f'http://apiserv:{api_port}/sendfile',json=payload)
+        res = requests.post(url=f'http://apiserv:{api_port}/sendfile',json=payload)
 
-            if res.ok:
+        if res.ok:
 
-                ret = res.json()
+            ret = res.json()
 
-                if ret['status'] == 0:
-                    
-                    st.success("File uploaded! Your reference ID is")
-                    st.header(ret['id'])
+            if ret['status'] == 0:
+                
+                st.success(f"File uploaded! Your reference ID is **{ret['id']}**")
+                
+                test_placeholder = st.container()
+                await print_test_cases(ret['id'])
+
+            else:
+                st.error(f"Something went wrong, please try again.\nError code : {ret['status']}")
+
+
+
+
+
+
+
+async def print_test_cases(code_id):
+
+
+    test_containers = []
+    test_cases_received = False
+    
+    while not test_cases_received:
+
+        res = requests.get(url=f'http://apiserv:{api_port}/getstatus/{code_id}')
+        res = res.json()
+
+        # Get number of test cases to create placeholder values
+
+        if 'execution' in res.keys():
+            test_results = res['execution']
+            if len(test_results) > 0:
+                test_cases_received = True
+                for i in range(len(test_results)):
+                    test_containers.append(st.empty())
+            else:
+                await asyncio.sleep(0.5)
+        
+    pending_count = [1 for i in range(len(test_results))]
+
+    while sum(pending_count) > 0:
+
+        res = requests.get(url=f'http://apiserv:{api_port}/getstatus/{code_id}')
+        res = res.json()
+
+        if 'execution' in res.keys():
+            test_results = res['execution']
+            for i in range(len(test_results)):
+                result = test_results[f'test case {i+1}']
+                if result['status'] != 'not_started':
+                    if result['retcode'] == 0:
+                        if result['status'] == 'successful':
+                            test_containers[i].success(f"Case {i} : Passed\nExpected : {result['expected_output']}\tGot : {result['exec_output']}")
+                        else:
+                            test_containers[i].warning(f"Case {i} : Failed\nExpected : {result['expected_output']}\tGot : {result['exec_output']}")
+                    else:
+                        test_containers[i].error(f"Case {i} : Execution failed. Traceback : {result['exec_output']}")
+                    pending_count[i] = 0
                 else:
-                    st.error(f"Something went wrong, please try again.\nError code : {ret['status']}")
-        except:
-            st.error("Something went wrong. Please try again")
+                    test_containers[i].info(f"Case {i} : Pending execution")
+        
+        await asyncio.sleep(2)
 
 
-def view_result():
+
+
+async def view_result():
 
     code_id = st.text_input("Enter your reference ID")
     if code_id != "":
         res = requests.get(url=f'http://apiserv:{api_port}/getstatus/{code_id}')
         res = res.json()
 
-        st.write(res)
 
         if res is not None:
 
             st.header('{} - {}'.format(res['id'],res['filename']))
             st.code(res['data'])
 
-            if 'execution' in res.keys():
-                test_results = res['execution']
-                for i in range(len(test_results)):
-
-                    result = test_results[f'test case {i+1}']
-                    if result['status'] != 'not_started':
-                        if result['retcode'] == 0:
-                            if result['status'] == 'successful':
-                                st.success(f"Case {i} : Passed\nExpected : {result['expected_output']}\tGot : {result['exec_output']}")
-                            else:
-                                st.warning(f"Case {i} : Failed\nExpected : {result['expected_output']}\tGot : {result['exec_output']}")
-                        else:
-                            st.error(f'Case {i} : Execution failed. Traceback below')
-                            st.error(res['exec_output'])
-                    else:
-                        st.info(f"Case {i} : Pending execution")
+            await print_test_cases(code_id)
 
 
-        # if res['status'] == 0:
-        #     st.header('{} - {}'.format(res['code_id'],res['filename']))
-        #     st.code(res['code_lines'],language='python')
 
-        #     test_results = res['execution']
 
-        #     for i in range(len(test_results)):
+async def main():
 
-        #         result = test_results[f'test case {i+1}']
-        #         if result['status'] != 'not_started':
-        #             if result['retcode'] == 0:
-        #                 if result['status'] == 'successful':
-        #                     st.success(f"Case {i} : Passed\nExpected : {result['expected_output']}\tGot : {result['exec_output']}")
-        #                 else:
-        #                     st.warning(f"Case {i} : Failed\nExpected : {result['expected_output']}\tGot : {result['exec_output']}")
-        #             else:
-        #                 st.error(f'Case {i} : Execution failed. Traceback below')
-        #                 st.error(res['exec_output'])
-        #         else:
-        #             st.info(f"Case {i} : Pending execution")
+    title_placeholder.title("Code Testing Platform")
+
+    sitepages = {
+        "Upload a submission" : upload_submission,
+        "View a result" : view_result
+    }
+
+    page_selection = st.sidebar.selectbox("I want to...",list(sitepages.keys()))
+    await sitepages[page_selection]()
 
 
 if __name__ == '__main__':
-
-        title_placeholder.title("Code Testing Platform")
-
-        sitepages = {
-            "Upload a submission" : upload_submission,
-            "View a result" : view_result
-        }
-
-        page_selection = st.sidebar.selectbox("I want to...",sitepages.keys())
-        sitepages[page_selection]()
+    asyncio.run(main())
