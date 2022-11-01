75673f3848a6ea9bd9b3884d48ca351872289579
A. Ganesh <a.ganesh.2@student.rug.nl>
2022-11-01 10:09:13 +0100

    Added stats to dashboard

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -6,7 +6,7 @@ import requests
 import os
 import asyncio
 import subprocess
-
+from datetime import datetime
 
 title_placeholder = st.empty()
 
@@ -26,12 +26,36 @@ service_placeholders = {
 
     }
 
+service_stats = {
+    'web1'                  : {'last_down' : "Never", 'events' : 0,'currently_down' : False},
+    'web2'                  : {'last_down' : "Never", 'events' : 0,'currently_down' : False},
+    'rabbitmq'              : {'last_down' : "Never", 'events' : 0,'currently_down' : False},
+    'mongodb-primary'       : {'last_down' : "Never", 'events' : 0,'currently_down' : False},
+    'mongodb-secondary'     : {'last_down' : "Never", 'events' : 0,'currently_down' : False},
+    'mongodb-arbiter'       : {'last_down' : "Never", 'events' : 0,'currently_down' : False},
+    'apiserv'               : {'last_down' : "Never", 'events' : 0,'currently_down' : False},
+    'testcaller'            : {'last_down' : "Never", 'events' : 0,'currently_down' : False},
+    'testrunner'            : {'last_down' : "Never", 'events' : 0,'currently_down' : False},
+    'dbhandler'             : {'last_down' : "Never", 'events' : 0,'currently_down' : False},
+
+    }
+
 async def ping_service(service):
+    global service_stats
     ret = subprocess.run(["ping", "-c", "1", service])
+    if ret.returncode != 0:
+        if not service_stats[service]['currently_down']:
+            service_stats[service]['currently_down'] = True
+            service_stats[service]['last_down'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            service_stats[service]['events'] +=1
+    else:
+        service_stats[service]['currently_down'] = False
+
+
     return ret.returncode
 
 async def show_dashboard():
-
+    global service_stats
     while True:
         for service in service_placeholders:
             retval = await ping_service(service)
@@ -42,6 +66,8 @@ async def show_dashboard():
                     stat.success("OK")
                 else:
                     stat.error("Fail")
+                stat.write(f"Last down : **{service_stats[service]['last_down']}**")
+                stat.write(f"Outage Events : **{service_stats[service]['events']}**")
 
         time.sleep(2)
 
